e654bd19fd24bcf202cb2fbc06acc5432c45a058
Sherman Leung <shermankleung@gmail.com>
2015-05-18 00:26:01 -0700

    added scoring functionality, may be some bugs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var port = process.env.PORT || 4000;
 var leader_num = 0;
 // Note: this JSON file is cached. So don't run any cron stuff on this script
 // var imageUrls = require('./image.json');
+var stopWords = require('stopwords').english;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -15,20 +16,24 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-// usernames which are currently connected to the chat
-var usernames = {};
+// scores which are currently connected to the chat
+var scores = {};
 var usernameArr = [];
 var numUsers = 0;
 
 // overhead to track the sentences and the words
 var freqMap = {};
+var sentenceMap = {};
 var sentence = "";
+var sentenceArr = [];
+var sentenceState = [];
 var remainingWords = {};
 
 var TIME_LIMIT = 30;
-var MIN_NUM_USERS = 4;
-// set the timer to 2 minutes
-var countdown = TIME_LIMIT;
+var MIN_NUM_USERS = 2;
+
+// set the timer to 30 seconds
+var countdown = 0;
 setInterval(function() {
   countdown--;
   io.sockets.emit('timer', { countdown: countdown });
@@ -45,6 +50,38 @@ io.on('connection', function (socket) {
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
+
+    // update the frequency map
+    if (data in freqMap) {
+      freqMap[data]++;
+    } else {
+      freqMap[data] = 1;
+    }
+
+    if (data in sentenceMap) {
+      if (freqMap[data] == 1) {
+        // first hit scores a point
+        scores[socket.username]++;
+        updateState(data);
+        socket.broadcast.emit('update score', {
+          username: socket.username,
+          word: data,
+          state: sentenceState,
+          scores: scores
+        });
+        socket.emit('hit word', {
+          scores: scores,
+          state: sentenceState,
+          word: data
+        })
+      } else {
+        // hit word, but no score
+        socket.emit('miss word', {
+          word: data
+        });
+      }
+    }
+
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
@@ -56,21 +93,21 @@ io.on('connection', function (socket) {
   socket.on('add user', function (username) {
     // we store the username in the socket session for this client
     socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
+    // add the client's username to the global list of scores and init to 0
+    scores[username] = 0;
     usernameArr.push(username);
     ++numUsers;
     addedUser = true;
     socket.emit('login', {
       numUsers: numUsers,
-      usernames: usernames,
+      scores: scores,
       sentence: sentence
     });
     // echo globally (all clients) that a person has connected
     socket.broadcast.emit('user joined', {
       username: socket.username,
       numUsers: numUsers,
-      usernames: usernames
+      scores: scores
     });
     if(numUsers < MIN_NUM_USERS){
       socket.emit('wait',{
@@ -91,13 +128,33 @@ io.on('connection', function (socket) {
     // set the owner of the sentence
     owner = data.username;
     sentence = data.sentence;
+    sentenceArr = sentence.split();
+    for (var i = 0; i < sentenceArr.length; i++) {
+      word = sentenceArr[i];
+      sentenceMap[word] = true;
+      sentenceState.push("");
+    }
+    var randWord = Math.floor((Math.random() * sentenceArr.length) + 1);
+    while (stopWords.indexOf(randWord) >= 0) {
+      randWord = Math.floor((Math.random() * sentenceArr.length) + 1);
+    }
+    updateState(word);
     // reset timer when the sentence is created
     countdown = TIME_LIMIT;
     // echo globally (all clients) that a sentence has been set
     socket.broadcast.emit('sentence set', {
-      owner: owner
+      owner: owner,
+      state: sentenceState
     });
   });
+
+  function updateState(word) {
+    for (var i = 0; i< sentenceArr.lenght; i++) {
+      if (sentenceArr[i] === word) {
+        sentenceState[i] = sentenceArr[i];
+      }
+    }
+  }
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function () {
@@ -115,15 +172,16 @@ io.on('connection', function (socket) {
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
-    // remove the username from global usernames list
+    // remove the username from global scores list
     if (addedUser) {
-      delete usernames[socket.username];
-      usernameArr.splice(usernameArr.indexOf(username),1);
+      delete scores[socket.username];
+      usernameArr.splice(usernameArr.indexOf(socket.username),1);
       --numUsers;
 
       // echo globally that this client has left
       socket.broadcast.emit('user left', {
         username: socket.username,
+        scores: scores,
         numUsers: numUsers
       });
 
--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 $(function() {
   // var VG = require('./VG.js');
-  var MIN_NUM_PLAYERS = 4;
+  var MIN_NUM_PLAYERS = 2;
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
   var COLORS = [
@@ -168,21 +168,7 @@ $(function() {
       options.fade = false;
       $typingMessages.remove();
     }
-    // if (username !== data.username) {
-    //   console.log("pushing " + data.message);
-    //   storeSentences(getSentences().push(data.message));
-    //   return;
-    // } else {
-    //   var mySentence = data.message;
-    //   var otherSentences = []// getSentences(); //this is an array of sentences
-    //   console.log("found: " + otherSentences);
-    //   $(otherSentences).each(function() {
-    //     var score = VG.bleu_score($(this), mySentence, 3);
-    //     console.log(score);
-    //   });
-    //   // run BLEU score here for my sentence against all the other ones
 
-    // }
     var $usernameDiv = $('<span class="username"/>')
       .text(data.username)
       .css('color', getUsernameColor(data.username));
@@ -368,7 +354,7 @@ $(function() {
     }
 
     addParticipantsMessage(data);
-    addAllUsers(data);
+    updateScores(data, false);
   });
 
   // Whenever the server emits 'new message', update the chat body
@@ -385,14 +371,14 @@ $(function() {
   socket.on('user joined', function (data) {
     log(data.username + ' joined');
     addParticipantsMessage(data);
-    addAllUsers(data);
+    updateScores(data);
   });
 
-  function addAllUsers(data) {
+  function updateScores(data, animated) {
     $scoreboard.html("");
-    for (user in data.usernames) {
+    for (user in data.scores) {
       var color = getUsernameColor(user);
-      var score = Math.floor(Math.random()*100);
+      var score = data.scores[user];
       if (user == username) {
         $scoreboard.append("<li style='color:"+ color +"'><strong>"+ user + ": "+ score + "</strong></li>");
       } else {
@@ -404,19 +390,29 @@ $(function() {
   // Whenever the server emits 'user left', log it in the chat body
   socket.on('user left', function (data) {
     log(data.username + ' left');
+    updateScores(data, false);
     addParticipantsMessage(data);
     removeChatTyping(data);
   });
 
   // Whenever the server tells us to wait
-  socket.on('wait', function(data) {
+  socket.on('wait', function (data) {
     showWaitingPage();
   });
 
+  // Whenever a user hits a word by themselves
+  socket.on('hit word', function (data) {
+    log("Congrats - you guessed " + data.word);
+    updateScores(data, true);
+  });
+
+  socket.on('update state', function(data) {
+    log(data.username + "correctly guessed " + data.word) + "!";
+    updateScores(data, true);
+  });
+
   // Whenever the server tells us that we can play the game
-  socket.on('start play', function(data){
-    console.log(data.leader);
-    console.log(username);
+  socket.on('start play', function (data){
     if(data.leader == username){
       showSentencePage();
     }else{
